ce5d9e628b0e763bafaf9f54481589a104166eb5
Nicholas Serra <nicks@ydekproductions.com>
2012-04-09 11:17:25 -0400

    package json and facebook code

--- main.js
+++ main.js
@@ -1,44 +1,30 @@
-var sys = require('sys'),
+var util = require('util'),
     postmark = require('postmark-api')("APIKEYHERE"),
-    Client = require('mysql').Client,
-    client = new Client();
- 
-client.user = 'singleyet';
-client.password = '***REMOVED***';
+    mysql = require('mysql'),
+    FacebookClient = require("facebook-client").FacebookClient
 
-client.connect(function(error, results) {
-    if(!error) {
-        setTable(client);
-    }
+var client = mysql.createClient({
+    database: 'singleyet',
+    //user: 'singleyet',
+    //password: '***REMOVED***'
+    user: 'root',
+    password: 'yd3k',
+    host: '10.0.30.2'
 });
 
-setTable = function(client)
-{
-    client.query('USE singleyet', function(error, results) {
-        if(error) {
-            console.log('Error: ' + error.message);
-            client.end();
-            return;
-        }
-        grabData(client);
-    });
-};
-
-grabData = function(client)
-{
-    client.query('SELECT * FROM follow', function iterate(error, results, fields) {
-        if(results.length > 0)
+client.query('SELECT * FROM followed', function iterate(error, results, fields) {
+    if(results.length > 0)
+    {
+        for (var i = 0; i < results.length; i++)
         {
-            for (var i = 0; i < results.length; i++)
-            {
-                checkResult(results[i]);
-            }
+            checkResult(results[i]);
         }
-    });
-    client.end();
-};
+    }
+});
+client.end();
 
 checkResult = function(result)
 {
     //connect to fb and check for new relationship
+    console.log(result);
 }

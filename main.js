e541b8b15dd3fdd0feb6a34f6ab79ae67e228575
Nicholas Serra <nicks@ydekproductions.com>
2012-04-09 18:13:09 -0400

    job ticker to kill mysql

--- main.js
+++ main.js
@@ -17,7 +17,8 @@ var util = require('util'),
             "timeout": 10000
         }
     ),
-    relationship_codes = {};
+    relationship_codes = {},
+    jobs = 0;
     
 //load relationship statusees
 client.query('SELECT * FROM `rel_status`', function iterate(error, results, fields) {
@@ -32,69 +33,90 @@ client.query('SELECT * FROM `rel_status`', function iterate(error, results, fiel
 
 singleYet = function(){
     client.query('SELECT * FROM `followed` JOIN `user` on followed.user_id = user.id', function iterate(error, results) {
-        if(results.length > 0)
-        {
+        if(results.length > 0){
             for (var i = 0; i < results.length; i++)
             {
                 checkResult(results[i]);
             }
         }
-    });
-    //client.end();
-}
-
-
-checkResult = function(db_result)
-{
-    params = {
-        'access_token': db_result['token']
-    };
-    
-    //connect to fb and check for new relationship 
-    fb_client.graphCall('/'+db_result['fb_id'], params)(function(fb_result) {
-                
-        if ('relationship_status' in fb_result && 
-            fb_result['relationship_status'] in relationship_codes && 
-            parseInt(relationship_codes[fb_result['relationship_status']]) != parseInt(db_result['rel_status'])
-        )
-        {
-            //got a change, push notification and send email, then change db
-            pushNotification(db_result, fb_result);
-            sendEmail(db_result, fb_result);
-            
-            //set new rel_status on table
-            client.query(
-                'UPDATE `followed` '+
-                'SET rel_status = ? '+
-                'WHERE id = ?',
-                [parseInt(relationship_codes[fb_result['relationship_status']]), db_result['id']]
-            );
+        else{
+            //no results
+            client.end();
         }
     });
 }
 
-pushNotification = function(db_data, fb_data)
-{
+checkResult = function(db_result){
+    params = {
+        'access_token': db_result['token']
+    };
+
+    //connect to fb and check for new relationship 
+    fb_client.graphCall('/'+db_result['fb_id'], params)(function(fb_result) {
+
+        if ('relationship_status' in fb_result && 
+            fb_result['relationship_status'] in relationship_codes && 
+            parseInt(relationship_codes[fb_result['relationship_status']]) != parseInt(db_result['rel_status'])){
+
+            //got a change, push notification and send email, then change db
+            pushNotification(db_result, fb_result);
+            sendEmail(db_result, fb_result);
+            updateRow(db_result, fb_result);
+        }
+    });
+}
+
+pushNotification = function(db_data, fb_data){
+    jobs++;
+
     //add row to notificaitons table
     client.query(
-      'INSERT INTO `notification`'+
-      'SET user_id = ?, followed_id = ?, message = ?, rel_status = ?',
-      [db_data['user_id'], db_data['id'], fb_data['name']+' is now '+fb_data['relationship_status'], parseInt(relationship_codes[fb_data['relationship_status']])]
+        'INSERT INTO `notification`'+
+        'SET user_id = ?, followed_id = ?, message = ?, rel_status = ?',
+        [db_data['user_id'], db_data['id'], fb_data['name']+' is now '+fb_data['relationship_status'], parseInt(relationship_codes[fb_data['relationship_status']])],
+        function(){
+            subtractAndCheck();
+        }
     );
 }
 
-sendEmail = function(db_data, fb_data)
-{
+sendEmail = function(db_data, fb_data){
+    jobs++;
+
     var to = db_data['email'],
         subject = fb_data['name']+' is now '+fb_data['relationship_status'],
         body = fb_data['name']+' is now '+fb_data['relationship_status'];
-    
+
     postmark.send({
         "From": "webmaster@singleyet.com",
         "To": to,
         "Subject": subject,
         "TextBody": body
+    }, function(){
+        subtractAndCheck();
     });
+}
+
+updateRow = function(db_data, fb_data){
+    jobs++;
+
+    //set new rel_status on table
+    client.query(
+        'UPDATE `followed` '+
+        'SET rel_status = ? '+
+        'WHERE id = ?',
+        [parseInt(relationship_codes[fb_data['relationship_status']]), db_data['id']],
+        function(){
+            subtractAndCheck();
+        }
+    );
+}
+
+subtractAndCheck = function(){
+    jobs--;
+    if (jobs == 0){
+        client.end();
+    }
 }
 
 singleYet();

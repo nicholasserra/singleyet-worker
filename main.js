a14c235c2130aa3c778eb31d2b3b4d3740f547ff
Nicholas Serra <nicks@ydekproductions.com>
2012-06-19 16:42:05 -0400

    More database fields wrong

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ client.query('SELECT * FROM `rel_status`', function iterate(error, results, fiel
 
 singleYet = function(){
     console.log('in main singleyet function');
-    client.query('SELECT user_id, followed.fb_id, rel_status, access_token, email, followed.id AS followed_id FROM `followed` JOIN `user` on followed.user_id = user.id', function iterate(error, results) {
+    client.query('SELECT user_id, followed.fb_id, rel_status_id, access_token, email, followed.id AS followed_id FROM `followed` JOIN `user` on followed.user_id = user.id', function iterate(error, results) {
         
         console.log('in client.query main function');
         
